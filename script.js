b5d52cf51d5050cf14ac06baaab27b85c3d71f74
Vitaly Zakatov <answer-0885@mail.ru>
2021-11-27 21:29:58 +0700

    выполнил усложненное домашнее задание

---
+++ script.js
@@ -0,0 +1,7 @@
+let num = 266219;
+
+let multi = num.toString().split('');
+
+let result = multi.reduce((acc, rec) => acc * rec); // Получаем произведение всех индексов массива
+
+console.log(String(result ** 3).slice(0, 2)); //полученное значение возводим в третью степень и выводим тоглько первые две цифры
