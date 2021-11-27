053d8899d2265b60e4b494e2f0cb115bdc1ac869
Vitaly Zakatov <answer-0885@mail.ru>
2021-11-27 21:42:45 +0700

    выполнил усложненное домашнее задание

--- script.js
+++ script.js
@@ -4,4 +4,4 @@ let multi = num.toString().split('');
 
 let result = multi.reduce((acc, rec) => acc * rec); // Получаем произведение всех индексов массива
 
-console.log(String(result ** 3).slice(0, 2)); //полученное значение возводим в третью степень и выводим тоглько первые две цифры
+console.log(String(result ** 3).slice(0, 2)); //полученное значение возводим в третью степень и выводим только первые две цифры
