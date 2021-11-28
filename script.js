714acc961a813edc691f8347b9b193d10ada7007
Vitaly Zakatov <answer-0885@mail.ru>
2021-11-28 15:21:18 +0700

    Все переменные вывел в консоль и подписал выполненные действия

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let num = 266219;
 
-let multi = num.toString().split('');
+let multi = num.toString().split(''); // переводим переменную типа number в массив
+
+console.log(multi);
 
 let result = multi.reduce((acc, rec) => acc * rec); // Получаем произведение всех индексов массива
 
