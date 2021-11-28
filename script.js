456ee51645a96ec56de94dc192015a39a904d048
Vitaly Zakatov <answer-0885@mail.ru>
2021-11-28 12:38:33 +0700

    добавил вывод в консоль произведения всех цифр числа

--- script.js
+++ script.js
@@ -4,4 +4,6 @@ let multi = num.toString().split('');
 
 let result = multi.reduce((acc, rec) => acc * rec); // Получаем произведение всех индексов массива
 
+console.log(result);
+
 console.log(String(result ** 3).slice(0, 2)); //полученное значение возводим в третью степень и выводим только первые две цифры
