7db294e2a20ed5c4c0dc2c9d8736a5cf3c414f05
Vitaly Zakatov <answer-0885@mail.ru>
2021-12-03 13:18:29 +0700

    Сделал замыкание

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
 'use strict'
 // Написать игровой бот.
 //"Загадывание случайного числа от 1 до 100"
+//Программа должны быть выполнена с помощью рекурсии, без единого цикла.
+//Загаданное число должно храниться «в замыкании».
 
-let randomNum = parseInt(Math.random() * 100);
 
-function isNum(num) {
-   return !isNaN(parseFloat(num)) && isFinite(num);
-}
+function rdm() {
+   let randomNum = parseInt(Math.random() * 100); // Загадываем число от одного до ста
 
-function guessNumber() {
-   let userNum = prompt('Угадай число от 1 до 100');
-   if (isNum(userNum)) {
-      userNum = +userNum;
+   function isNum(num) {
+      return !isNaN(parseFloat(num)) && isFinite(num); // проверяем чтобы введённое значение было числом
+   };
 
-      if (userNum > randomNum) {
-         alert('Загаданное число меньше');
-      } else if (userNum < randomNum) {
-         alert('Загаданное число больше');
-      } else if (userNum === randomNum) {
-         alert('Поздравляю, Вы угадали!!!');
-         return userNum;
+   function guessNumber() {
+      let userNum = prompt('Угадай число от 1 до 100');
+      if (isNum(userNum)) {
+         userNum = +userNum;
+
+         if (userNum > randomNum) {
+            alert('Загаданное число меньше');
+         } else if (userNum < randomNum) {
+            alert('Загаданное число больше');
+         } else if (userNum === randomNum) {
+            alert('Поздравляю, Вы угадали!!!');
+            return userNum;
+         }
+      } else if (userNum === null) {
+         alert('Игра окончена');
+         return;
+      } else {
+         alert('Введите число!');
       }
-   } else if (userNum === null) {
-      alert('Игра окончена');
-      return;
-   } else {
-      alert('Введите число!');
+      return guessNumber();
    }
-   return guessNumber();
+   console.log(guessNumber());
 }
-
-console.log(guessNumber());
+rdm();
