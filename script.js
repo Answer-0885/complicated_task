3a76fa9c52ada09f50fe52be99daba3a90cad43d
Vitaly Zakatov <answer-0885@mail.ru>
2021-12-03 16:04:16 +0700

    Выполнил игру с ботом 'Угадай число'

--- script.js
+++ script.js
@@ -2,13 +2,17 @@
 // Написать игровой бот.
 //"Загадывание случайного числа от 1 до 100"
 
+let lives;
 
 function rdm() {
    let randomNum = parseInt(Math.random() * 100); // Загадываем число от одного до ста
+   lives = 9;
+
 
    function isNum(num) {
       return !isNaN(parseFloat(num)) && isFinite(num); // проверяем чтобы введённое значение было числом
    };
+
 
    function guessNumber() {
       let userNum = prompt('Угадай число от 1 до 100');
@@ -16,12 +20,16 @@ function rdm() {
          userNum = +userNum;
 
          if (userNum > randomNum) {
-            alert('Загаданное число меньше');
+            alert('Загаданное число меньше, осталось попыток ' + lives);
          } else if (userNum < randomNum) {
-            alert('Загаданное число больше');
+            alert('Загаданное число больше, осталось попыток ' + lives);
          } else if (userNum === randomNum) {
-            alert('Поздравляю, Вы угадали!!!');
-            return userNum;
+            if (confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?') === true) {
+               rdm()
+            } else {
+               alert('Спасибо за игру, дружок!)')
+               return userNum;
+            }
          }
       } else if (userNum === null) {
          alert('Игра окончена');
@@ -29,8 +37,19 @@ function rdm() {
       } else {
          alert('Введите число!');
       }
+      if (lives > 0) {
+         lives--;
+      } else {
+         if (confirm('Попытки закончились, хотите сыграть еще?') === true) {
+            rdm();
+         } else {
+            alert('Спасибо за игру, дружок!)')
+            return userNum;
+         }
+      }
       return guessNumber();
    }
    console.log(guessNumber());
+
 }
 rdm();
