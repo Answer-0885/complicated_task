7f6390659ce91c678f53ddea856c54cdc6c47c23
Vitaly Zakatov <answer-0885@mail.ru>
2021-12-08 00:42:32 +0700

    Усложнённая домашка по 9 уроку

--- script.js
+++ script.js
@@ -1,30 +1,102 @@
-'use strict';
-// Необходимо выполнить в отдельном js файле, подключенному к отдельной HTML странице
-// 1) Создать массив week и записать в него дни недели в виде строк
-// Вывести на экран все дни недели
-// Каждый из них с новой строчки
-// Выходные дни - курсивом
-// Текущий день - жирным шрифтом(использовать объект даты)
-const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-const day = document.querySelector('.fordays'); // Получаем div куда всё будем вставлять
-const todayDay = new Date();
+// 'use strict';
+// 1) Выведите на страницу текущую дату и время в 2 - х форматах:
 
-const days = () => {
-   week.forEach((item, i) => {
-      let newdiv = document.createElement('div'); // Для каждого элемента week создаём div
-      if (i === +todayDay.getDay() - 1) { // Если текущий день недели то от номера текущей даты отнимаем единицу, так как массив у нас начинается с нуля
-         console.log(todayDay.getDay());
-         newdiv.classList.add('today'); // Добавляем класс (делаем жирным)
-         newdiv.textContent = week[i]; // Вставляем текст
-      }
-      if (item == 'Суббота' || item == 'Воскресенье') { // Если выходные то
-         newdiv.classList.add('italic'); // Делаем дополнительно курсивом (Даже если уже жирным выделено)
-         newdiv.textContent = week[i]; // Вставляем текст
+//  a)'Сегодня Вторник, 4 февраля 2020 года, 21 час 5 минут 33 секунды'
+// Для вывода в формате(а) напишите функцию, которая будет менять склонение слов в зависимости от числа, "час, часов, часа"
+
+let d = new Date();
+let hours = d.getHours();
+let mins = d.getMinutes();
+let sec = d.getSeconds();
+
+let wordForm = function (num, word) {
+   cases = [2, 0, 1, 1, 1, 2];
+   return word[(num % 100 > 4 && num % 100 < 20) ? 2 : cases[(num % 10 < 5) ? num % 10 : 5]]; // проверка, чтобы подобрать правильное окончание.
+}
+resultHours = hours + wordForm(hours, [' час ', ' часа ', ' часов ']);
+resultMins = mins + wordForm(mins, [' минута ', ' минуты ', ' минут ']);
+resultSec = sec + wordForm(sec, [' секунда ', ' секунды ', ' секунд ']);
+
+let day = new Array("Воскресенье", "Понедельник", "Вторник",
+   "Среда", "Четверг", "Пятница", "Суббота");
+
+let month = new Array("января", "февраля", "марта", "апреля", "мая", "июня",
+   "июля", "августа", "сентября", "октября", "ноября", "декабря");
+
+alert("Сегодня " + (day[d.getDay()] + " " + d.getDate() + " " + month[d.getMonth()] +
+   " " + d.getFullYear() + " г. " + resultHours + resultMins + resultSec));
+
+
+
+//  б)'04.02.2020 - 21:05:33'
+// Для вывода в формате(б) напишите функцию, которая будет добавлять 0 перед значениями которые состоят из одной цифры(из 9: 5: 3 1.6 .2019 сделает 09: 05: 03 01.06 .2019)
+let forDays = document.querySelector('.fordays');
+
+
+function clock() {
+   Data = new Date();
+   year = Data.getFullYear();
+   month = Data.getMonth();
+   day = Data.getDate();
+   hour = Data.getHours();
+   minutes = Data.getMinutes();
+   seconds = Data.getSeconds();
+
+   // проверка если цифра до 9 включительно, то прибавляем 0 спереди
+   function addZero(num) {
+      if (num >= 0 && num <= 9) {
+         return '0' + num;
       } else {
-         newdiv.textContent = week[i]; // Если не текущий день и не выходные, то просто вставляем текст без стилей
+         return num;
       }
-      day.appendChild(newdiv); // Добавляем новый див в div day
+   }
 
-   });
-};
-days(); // Вызываем функцию;
+   switch (month) {
+      case 0:
+         fMonth = "1";
+         break;
+      case 1:
+         fMonth = "2";
+         break;
+      case 2:
+         fMonth = "3";
+         break;
+      case 3:
+         fMonth = "4";
+         break;
+      case 4:
+         fMonth = "5";
+         break;
+      case 5:
+         fMonth = "6";
+         break;
+      case 6:
+         fMonth = "7";
+         break;
+      case 7:
+         fMonth = "8";
+         break;
+      case 8:
+         fMonth = "9";
+         break;
+      case 9:
+         fMonth = "10";
+         break;
+      case 10:
+         fMonth = "11";
+         break;
+      case 11:
+         fMonth = "12";
+         break;
+   }
+
+
+   currentTime = "Текущее время: " + addZero(day) + "." + addZero(fMonth) + "." + addZero(year) + " - " + addZero(hour) + ":" + addZero(minutes) + ":" + addZero(seconds)
+
+   // Вывод
+   console.log(currentTime);
+}
+setInterval(clock, 1000);
+
+clock();
+forDays.textContent = currentTime;
