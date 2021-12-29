5aa3d28a734de986bbc97bbbedcc3089a6b30ef2
Vitaly Zakatov <answer-0885@mail.ru>
2021-12-29 16:58:07 +0700

    Усложнённая домашка по 22 уроку

--- Кнопка и клетка/scripts/script.js
+++ Кнопка и клетка/scripts/script.js
@@ -2,57 +2,59 @@
 
 let blocks = document.getElementsByClassName('block');
 let square = document.querySelector('.square-body');
-
+let cloneArray = square.slice(0);
+console.log(square);
+console.log(cloneArray);
 
 // Перемещение блока влево
 const moveLeft = (e) => {
-   let prevElem = e.previousElementSibling;
-   if (prevElem) {
-      e.parentNode.insertBefore(e, prevElem);
-   }
+    let prevElem = e.previousElementSibling;
+    if (prevElem) {
+        e.parentNode.insertBefore(e, prevElem);
+    }
 }
 // Перемещение блока вправо
 const moveRight = (e) => {
-   let nextElem = e.nextElementSibling;
-   if (nextElem) {
-      e.parentNode.insertBefore(nextElem, e);
-   }
+    let nextElem = e.nextElementSibling;
+    if (nextElem) {
+        e.parentNode.insertBefore(nextElem, e);
+    }
 }
 // Перемещение блока вверх
 const moveUp = (e) => {
-   let id = Array.from(blocks).findIndex(currentValue => currentValue == e);
-   let validId = id - 5;
-   if (validId >= 0) {
-      blocks[validId].after(blocks[id]);
-      blocks[id].after(blocks[validId]);
-   }
+    let id = Array.from(blocks).findIndex(currentValue => currentValue == e);
+    let validId = id - 5;
+    if (validId >= 0) {
+        blocks[validId].after(blocks[id]);
+        blocks[id].after(blocks[validId]);
+    }
 }
 //Перемещение блока вниз
 const moveDown = (e) => {
-   let id = Array.from(blocks).findIndex(currentValue => currentValue == e);
-   let validId = id + 5;
-   if (validId <= 25) {
-      blocks[id].after(blocks[validId]);
-      blocks[validId].after(blocks[id]);
-   }
+    let id = Array.from(blocks).findIndex(currentValue => currentValue == e);
+    let validId = id + 5;
+    if (validId <= 25) {
+        blocks[id].after(blocks[validId]);
+        blocks[validId].after(blocks[id]);
+    }
 }
 
 document.body.addEventListener('click', (e) => {
-   if (e.target.closest('.left') && e.target.closest('.block')) {
-      let btn = e.target.closest('.left') && e.target.closest('.block');
-      moveLeft(btn);
-   } else if (e.target.closest('.right') && e.target.closest('.block')) {
-      let btn = e.target.closest('.right') && e.target.closest('.block');
-      moveRight(btn);
-   } else if (e.target.closest('.top') && e.target.closest('.block')) {
-      let btn = e.target.closest('.top') && e.target.closest('.block');
-      moveUp(btn);
-   } else if (e.target.closest('.bottom') && e.target.closest('.block')) {
-      let btn = e.target.closest('.bottom') && e.target.closest('.block');
-      moveDown(btn);
-   } else if (e.target.className === 'btn-reset') {
-      square.innerHTML = '';
-      square.innerHTML = `<div class="block">
+    if (e.target.closest('.left') && e.target.closest('.block')) {
+        let btn = e.target.closest('.left') && e.target.closest('.block');
+        moveLeft(btn);
+    } else if (e.target.closest('.right') && e.target.closest('.block')) {
+        let btn = e.target.closest('.right') && e.target.closest('.block');
+        moveRight(btn);
+    } else if (e.target.closest('.top') && e.target.closest('.block')) {
+        let btn = e.target.closest('.top') && e.target.closest('.block');
+        moveUp(btn);
+    } else if (e.target.closest('.bottom') && e.target.closest('.block')) {
+        let btn = e.target.closest('.bottom') && e.target.closest('.block');
+        moveDown(btn);
+    } else if (e.target.className === 'btn-reset') {
+        square.innerHTML = '';
+        square.innerHTML = `<div class="block">
                 <div class="block-number">1</div>
                 <div class="block-btn">
                     <div class="arrow left"><img src="img/arrow-left.svg" alt=""></div>
@@ -277,5 +279,5 @@ document.body.addEventListener('click', (e) => {
                     <div class="arrow bottom"><img src="img/arrow-down.svg" alt=""></div>
                 </div>
             </div>`;
-   }
+    }
 });
